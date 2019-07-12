bd38aafaeaf402d07eed1aaa2d54b747c8c240cc
ddrdushy <ddrdushy@yahoo.com>
2019-07-12 23:20:56 +0800

    Updated for dynamic text log loading

--- LogViewer_V2/Form1.cs
+++ LogViewer_V2/Form1.cs
@@ -22,6 +22,7 @@ namespace LogViewer_V2
             dt = new DataTable();
             Type type = typeof(logClass);
             var properties = type.GetProperties();
+
             CheckBox box;
             int count = 0;
             int count2 = 20;
@@ -48,34 +49,24 @@ namespace LogViewer_V2
                 count++;
             }
 
+
             gb1.AutoSize = true;
             dataGridView1.Location = new Point(5, count2 + 75);
+            dataGridView1.ScrollBars = ScrollBars.Both;
             dataGridView1.Anchor = (AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right | AnchorStyles.Left);
+            dataGridView1.DataSource = dt;
+
             btn_exportToExcel.AutoSize = true;
             btn_exportToExcel.Anchor = (AnchorStyles.Top|AnchorStyles.Right);
             btn_exportToExcel.Size = new Size(btn_submit.Width, gb1.Height - 8);
+            
 
         }
 
         private void Btn_submit_Click(object sender, EventArgs e)
         {
+            
             String FileName = txt_fileName.Text;
-
-            //string[] lines = File.ReadAllLines(FileName);
-
-
-            /* foreach (String line in lines){
-                 //Console.WriteLine(line.Split(' ')[0]);
-                 //Console.WriteLine(line.Substring(line.IndexOf("{")));
-
-                 //dynamic jsonOb = JsonConvert.DeserializeObject(line.Replace(line.Split(' ')[0], "").Replace(line.Split(' ')[1], "").Trim());
-                 //MessageBox.Show(line.Substring(line.IndexOf("{")));
-                 logClass log = JsonConvert.DeserializeObject<logClass>(line.Substring(line.IndexOf("{")));
-                 log.logDate = line.Split(' ')[0];
-
-                 dt.Rows.Add(log.GetAllPropertyValues().ToArray());
-
-             }*/
 
             using (FileStream fs = File.Open(FileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             {
@@ -90,13 +81,12 @@ namespace LogViewer_V2
                             log.logDate = line.Split(' ')[0];
 
                             dt.Rows.Add(log.GetAllPropertyValues().ToArray());
+                            dataGridView1.Refresh();
                         }
                     }
                 }
             }
 
-            dataGridView1.DataSource = dt;
-           
         }
 
         public void SampleCheckChangedHandler(object objSender, EventArgs ea)
